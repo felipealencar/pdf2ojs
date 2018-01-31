5bb4eb2d9701f28b4767925bdfef85f34785edfd
Gustavo de Araújo <inf.araujo@gmail.com>
2018-01-31 08:44:48 -0200

    Add license

--- src/main/java/com/pdf2ojs/main/Main.java
+++ src/main/java/com/pdf2ojs/main/Main.java
@@ -1,19 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2018 gustavo
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package com.pdf2ojs.main;
+
 
 import com.pdf2ojs.view.ExtractorView;
 
 /**
- *
- * @author gustavo
- */
+* Main Class of the PDF2OJS
+* 
+* <P>This file just call the Extractor View.
+*  
+* @author Gustavo Araújo
+* @version 1.0
+*/
 public class Main {
 
     /**
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
